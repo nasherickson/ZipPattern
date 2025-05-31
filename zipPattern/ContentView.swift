1ea2600c398548c8cf1c77f3bb5ad037ef8a0c0d
nasherickson <nasherickson.ops@outlook.com>
2025-05-30 19:19:42 -0700

    Yay!

--- zipPattern/ContentView.swift
+++ zipPattern/ContentView.swift
@@ -32,7 +32,7 @@ struct ContentView: View {
                 }) {
                     Image(systemName: "chevron.right")
                 }
-                Button("Open PDF") {
+                Button("Open PDF YAY!") {
                     showFilePicker = true
                 }
                 Spacer()
